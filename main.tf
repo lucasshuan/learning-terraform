47e1b54308f673c87203f02b831ff416ad4cf810
lucasshuan <jlsrocha1@gmail.com>
2024-04-10 15:33:44 -0300

    variables and zone added

--- main.tf
+++ main.tf
@@ -8,10 +8,29 @@ terraform {
 }
 
 provider "google" {
-  project = "hardy-canyon-419900"
+  credentials = file(var.credentials_file)
+  project = var.project
+  region = var.region
+  zone = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
